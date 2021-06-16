84568040cecec1b7ade3aa2be359405b65d3d5d8
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-16 08:16:45 -0700

    expand ticket business logic + ticket details in UI

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@
 
 
 // Business Logic
+function Cinemagic() {
+  this.tickets = {};
+}
+
 function Movie (title1, title2) {
   this.title1= title1
   this.title2= title2
@@ -20,12 +24,15 @@ function Age (showTime, adult, senior) {
   this.senior= senior
 }
 
-function Ticket (adult, senior, cost) {
-  this.adult= adult
-  this.senior= senior
+
+//business logic for ticket printedTicket
+function Ticket (ageId, cost) {
+  this.age= ageId
   this.cost= cost
 }
-
+Ticket.prototype.movieTicket = function () {
+  return this.chosenTitle + " " + this.time + " " + this.age;
+};
 
 
 
@@ -33,7 +40,14 @@ function Ticket (adult, senior, cost) {
 
 
 // UI Logic
+let Cinemagic = new Cinemagic();
 
+function displayTicketDetails(cinemagicTicketsOutput){
+  let movieTicket = $("ul#printedTicket");
+  let htmlForTicketInfo = "";
+  Object.keys(cinemagicTicketsOutput.tickets).forEach(function(key);
+    const ticket = cinemagicTicketsOutput. 
+}
 
 
 
@@ -43,28 +57,29 @@ $(document).ready(function() {
 
 
     const baseCost= 10;
-    let finalTicketCost = 0 //unsure of value to list initially
+    let finalTicketCost = 0 
     let movieTitle = $("#film").val();
     let time = $("input:radio[name=show-time]:checked").val();
     let age = $("ageId").val();
 
-    if (movieTitle === title1) && (time === matinee) && age === adult){
-      finalTicketCost = baseCost -3;
-    }else if (movieTitle === title1) && (time === matinee) && age === senior){
-      finalTicketCost = baseCost -5;
-    }else if (movieTitle === title1) && (time === evening) && age === adult){
-      finalTicketCost = baseCost;
-    {else if (movieTitle === title1) && (time === evening) && age === senior){
-      finalTicketCost = baseCost -2;
-    }else if (movieTitle === title2) && (time === matinee) && age === adult){
-      finalTicketCost = baseCost +2;
-    }else if (movieTitle === title2) && (time === matinee) && age === senior){
-      finalTicketCost = baseCost;
-    }else if (movieTitle === title2) && (time === evening) && age === adult){
-      finalTicketCost = baseCost +5;
-    }else if (movieTitle === title2) && (time === evening) && age === senior){
-      finalTicketCost = baseCost +3;
+      if (movieTitle === title1) && (time === matinee) && age === adult){
+        finalTicketCost = baseCost -3;
+      }else if (movieTitle === title1) && (time === matinee) && age === senior){
+        finalTicketCost = baseCost -5;
+      }else if (movieTitle === title1) && (time === evening) && age === adult){
+        finalTicketCost = baseCost;
+      {else if (movieTitle === title1) && (time === evening) && age === senior){
+        finalTicketCost = baseCost -2;
+      }else if (movieTitle === title2) && (time === matinee) && age === adult){
+        finalTicketCost = baseCost +2;
+      }else if (movieTitle === title2) && (time === matinee) && age === senior){
+        finalTicketCost = baseCost;
+      }else if (movieTitle === title2) && (time === evening) && age === adult){
+        finalTicketCost = baseCost +5;
+      }else if (movieTitle === title2) && (time === evening) && age === senior){
+        finalTicketCost = baseCost +3;
     
+    let 
 
     }
   });
