557e5d2ece72477936391f9a8fd12011c97e9572
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-15 22:33:57 -0700

    beging business logic- create functions for each input & cost

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,27 @@
 
 
 // Business Logic
+function Movie (title1, title2) {
+  this.title1= title1
+  this.title2= title2
+}
 
+function Time (film, matinee, evening) {
+  this.chosenTitle= film 
+  this.matinee= matinee
+  this.evening= evening
+}
+
+function Age (showTime, adult, senior) {
+  this.time= showTime
+  this.adult= adult
+  this.senior= senior
+}
+
+function Ticket (ageId, cost) {
+  this.age= ageId
+  this.cost= cost
+}
 
 
 
@@ -19,6 +39,6 @@
 $(document).ready(function() {
   $("").submit(function(event){
     event.preventDefault();
-    
+
   });
 });
