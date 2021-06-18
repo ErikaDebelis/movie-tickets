696ae4efae56dab836f346fe9e540a66d592add0
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-17 20:05:38 -0700

    WIP: show-ticket div not hiding + bugfixed branching stmts

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
 // Business Logic
 function Cinemagic() {
   this.tickets = {};
+  this.currentId = 0;
 }
 
 function Movie (title1, title2) {
@@ -25,62 +26,92 @@ function Age (showTime, adult, senior) {
 }
 
 
-//business logic for ticket printedTicket
-function Ticket (ageId, cost) {
-  this.age= ageId
-  this.cost= cost
-}
-Ticket.prototype.movieTicket = function () {
+//business logic for tickets
+Cinemagic.prototype.addTicket = function (ticket) {
+  ticket.id = this.assignTicketId();
+  this.tickets[ticket.id] = ticket;
+};
+
+Cinemagic.prototype.assignTicketId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+Cinemagic.prototype.findTicket = function (id) {
+  if (this.tickets[id] != undefined) {
+    return this.tickets[id];
+  }
+  return false;
+};
+Cinemagic.prototype.movieTicket = function () {
   return this.chosenTitle + " " + this.time + " " + this.age;
 };
 
 
-
-
-
-
 // UI Logic
 let Cinemagic = new Cinemagic();
-
+let movieTicket = $("ul#printedTicket");
+let htmlForTicketInfo = "";
 function displayTicketDetails(cinemagicTicketsOutput){
-  let movieTicket = $("ul#printedTicket");
-  let htmlForTicketInfo = "";
-  Object.keys(cinemagicTicketsOutput.tickets).forEach(function(key);
-    const ticket = cinemagicTicketsOutput. 
+  Object.keys(cinemagicTicketsOutput.tickets).forEach(function (key) {
+    const ticket = cinemagicTicketsOutput.findTicket(key);
+    htmlForTicketInfo += "<ul id=" + ticket.output + ">" + ticket.chosenTitle + " " + ticket.time + ticket.age +"</ul>";
+  });
+
+  movieTicket.html(htmlForTicketInfo);
 }
 
+$("#show-ticket").hide();
+$(".movie-title").hide();
+$(".showtime").hide();
+$(".age").hide();
+$(".cost").hide();
 
-
+// function showTicket(TicketId) {
+//   const ticket = addressBook.findTicket(TicketId);
+//   $("#show-ticket").show();
+//   $(".movie-title").html(ticket.movieTitle);
+//   $(".showtime").html(ticket.time);
+//   $(".age").html(ticket.age);
+//   $(".cost").html(Ticket.finalTicketCost);
+//   $(".ticket").html(displayTicketDetails(cinemagic.tickets));
+// }
+// function attachTicketListeners() {
+//   $("ul#ticket-output").on("click", "li", function () {
+//     showTicket(this.id);
+//     displayTicketDetails(cinemagic);
+//   });
+// }
 $(document).ready(function() {
+  // attachTicketListeners();
   $("button.btn-warning").click(function(event){
     event.preventDefault();
 
-
     const baseCost= 10;
-    let finalTicketCost = 0 
+    let finalTicketCost = 0; 
     let movieTitle = $("#film").val();
     let time = $("input:radio[name=show-time]:checked").val();
     let age = $("ageId").val();
 
-      if (movieTitle === title1) && (time === matinee) && age === adult){
-        finalTicketCost = baseCost -3;
-      }else if (movieTitle === title1) && (time === matinee) && age === senior){
-        finalTicketCost = baseCost -5;
-      }else if (movieTitle === title1) && (time === evening) && age === adult){
-        finalTicketCost = baseCost;
-      {else if (movieTitle === title1) && (time === evening) && age === senior){
-        finalTicketCost = baseCost -2;
-      }else if (movieTitle === title2) && (time === matinee) && age === adult){
-        finalTicketCost = baseCost +2;
-      }else if (movieTitle === title2) && (time === matinee) && age === senior){
-        finalTicketCost = baseCost;
-      }else if (movieTitle === title2) && (time === evening) && age === adult){
-        finalTicketCost = baseCost +5;
-      }else if (movieTitle === title2) && (time === evening) && age === senior){
-        finalTicketCost = baseCost +3;
-    
-    let 
-
+    if (movieTitle === title1 && time === matinee && age === adult){
+      finalTicketCost += baseCost -3;
+    }else if (movieTitle === title1 && time === matinee && age === senior){
+      finalTicketCost += baseCost -5;
+    }else if (movieTitle === title1 && time === evening && age === adult){
+      finalTicketCost += baseCost;
+    }else if (movieTitle === title1 && time === evening && age === senior){
+      finalTicketCost += baseCost -2;
+    }else if (movieTitle === title2 && time === matinee && age === adult){
+      finalTicketCost += baseCost +2;
+    }else if (movieTitle === title2 && time === matinee && age === senior){
+      finalTicketCost += baseCost;
+    }else if (movieTitle === title2 && time === evening && age === adult){
+      finalTicketCost = baseCost +5;
+    }else if (movieTitle === title2 && time === evening && age === senior){
+      finalTicketCost = baseCost +3;
+  
+    let newTickets = new Tickets(movieTitle, time, age, finalTicketCost);
+    Cinemagic.addTicket(newTickets);
+    displayTicketDetails(cinemagic);
     }
   });
 });
